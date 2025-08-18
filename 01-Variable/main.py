60d2f3298b0400ba1543b0f140be23089457cb9d
Jeremiah Lengkong <jerlenh12@gmail.com>
2025-08-18 13:01:16 +0800

    Menambahkan materi yang sudah di pelajari

---
+++ 01-Variable/main.py
@@ -0,0 +1,11 @@
+# variabel adalah tempat menyimpan data
+
+nama = "Jere"
+umur = 17
+tinggi = 160.3
+mahasiswa = True
+
+# Hal yang perlu diperhatikan saat penamaan variabel di python 
+# Tidak boleh diawali angka 
+# Tidak boleh mengandung spasi 
+# python itu case sensitive (huruf besar dan kecil bakal di anggap berbeda)
---
+++ 02-Tipe-Data/main.py
@@ -0,0 +1,35 @@
+# Beberapa Tiper Data Python :
+
+#1 - Integer (int) -> Angka bulat dan tidak menggunakan koma(.) :
+# Contoh :
+umur = 17
+tahun = 2025
+
+#2 - Float -> Angka desimal dan menggunakan koma(.) :
+# Contoh :
+tinggi = 160.32
+berat = 47.45
+
+#3 - String (Str) -> Teks/kalimat, yang diapit menggunakan tanda kutip (" ") :
+# Contoh :
+nama = "Jeremiah"
+hobi = "Coding"
+
+#4 - Boolean (bool) -> Hanya berisi True/False :
+# Contoh :
+sudah_makan = True
+sudah_tidur = False
+
+#5 - Kompleks (Complex) -> Tipe data ini dipakai untuk angka bilangan kompleks (ada bagian real dan imajiner ). di python, bilangan kompleks ditulis dedngan huruf j sebagai imajiner
+# Contoh :
+z = 3 + 5j #Output : (3+5j)
+# Bagian real dan imajiner bisa dipisahkan 
+print(z.real)
+print(z.imag)
+
+# Python juga bisa menyimpan tipe data dari bahasa pemograman C dengan menggunakan library "Ctypes"
+# Syntax untuk mengecek suatu variabel yang menyimpan/memiliki tipe data apa :
+# Contoh :
+data = "Jeremiah - Lengkong"
+print(type(data)) #Outpunya : <class 'str'> | yang berarti variabel bernama data menyimpan/memiliki tipe data string(str).
+
